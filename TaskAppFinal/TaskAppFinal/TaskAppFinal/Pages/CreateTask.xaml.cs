ab46178547a4303280bdde90e57bfc04113588a9
David Pilarski <david.pilarski@hotmail.com>
2023-03-28 13:59:46 -0400

    Implement Picker and Switch widgets

--- TaskAppFinal/TaskAppFinal/TaskAppFinal/Pages/CreateTask.xaml.cs
+++ TaskAppFinal/TaskAppFinal/TaskAppFinal/Pages/CreateTask.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using TaskAppFinal.Models;
 using TaskAppFinal.Services;
 using Xamarin.Forms;
 namespace TaskAppFinal.Pages
@@ -9,13 +10,36 @@ namespace TaskAppFinal.Pages
 		public CreateTask ()
 		{
 			InitializeComponent ();
+            fetchUsers();
 		}
 
-        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        private async void fetchUsers()
         {
             try
             {
-                await AppState.GetInstance().ApiClient.CreateTask(Description.Text, AssignedToUid.Text);
+                var users = await AppState.GetInstance().ApiClient.GetAllUsers();
+                AssignedToUser.ItemsSource = users;
+            }
+            catch (Exception e)
+            {
+                Message.Text = e.Message;
+            }
+        }
+
+        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            var user = (User)AssignedToUser.SelectedItem;
+
+            if (user == null)
+            {
+                Message.Text = "Assigned to user field is required";
+                Message.TextColor = Color.Red;
+                return;
+            }
+
+            try
+            {
+                await AppState.GetInstance().ApiClient.CreateTask(Description.Text, user.UID);
                 Message.Text = "Task created successfully";
                 Message.TextColor = Color.Green;
             }
--- TaskAppFinal/TaskAppFinal/TaskAppFinal/Pages/EditTask.xaml.cs
+++ TaskAppFinal/TaskAppFinal/TaskAppFinal/Pages/EditTask.xaml.cs
@@ -16,14 +16,17 @@ namespace TaskAppFinal.Pages
 			InitializeComponent ();
             _task = task;
 			Description.Text = task.Description;
-			Done.IsChecked = Boolean.Parse(task.Done);
+            SwitchDone.IsToggled = Boolean.Parse(task.Done);
 		}
 
-        async void Button_Clicked(System.Object sender, System.EventArgs e)
+        async void SwitchDone_PropertyChanged(System.Object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
+            if (SwitchDone.IsToggled == Boolean.Parse(_task.Done)) return;
+
             try
             {
-                await AppState.GetInstance().ApiClient.UpdateTask(_task.TaskUid, Done.IsChecked);
+                await AppState.GetInstance().ApiClient.UpdateTask(_task.TaskUid, SwitchDone.IsToggled);
+                _task.Done = SwitchDone.IsToggled.ToString();
                 Message.Text = "Task updated successfully";
                 Message.TextColor = Color.Green;
             }
